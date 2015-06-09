7b44d1e9848e57378a40ed06823f6de908414db8
MykhailoBrodskyi <mykhailo.brodskyi@globallogic.com>
2015-06-09 15:04:48 +0300

    Updated ParkingSpot Model, new requiremnts for impl

--- src/main/java/com/parksystem/model/ParkingSpot.java
+++ src/main/java/com/parksystem/model/ParkingSpot.java
@@ -1,22 +1,77 @@
 package com.parksystem.model;
 
-
 import com.parksystem.model.Vehicle;
 import com.parksystem.model.VehicleSize;
 
 public class ParkingSpot {
-	
+
 	private Vehicle vehicle;
 	private VehicleSize spotSize;
 	private int row;
 	private int spotNumber;
 	private Level level;
-	
+
 	public ParkingSpot(Level lvl, int r, int n, VehicleSize sz) {
 		level = lvl;
 		row = r;
 		spotNumber = n;
 		spotSize = sz;
+	}
+
+	/*
+	 * In this method you have to check: 1. is this Spot available 2. can this
+	 * vehicle fit in this spot
+	 */
+	public boolean canFitVehicle(Vehicle vehicle) {
+		return isAvailable() && vehicle.canFitInSpot(this);
+	}
+
+	public boolean isAvailable() {
+		return vehicle == null;
+	}
+
+	/* Park vehicle in this spot. */
+	public boolean park(Vehicle v) {
+		if (!canFitVehicle(v)) {
+			return false;
+		}
+		vehicle = v;
+		vehicle.parkInSpot(this);
+		return true;
+	}
+
+	/*
+	 * TBD: you have to implement removing vehicle from spot, and notify level
+	 * that a new spot is available
+	 */
+	public void removeVehicle() {
+
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getSpotNumber() {
+		return spotNumber;
+	}
+
+	public VehicleSize getSize() {
+		return spotSize;
+	}
+
+	public void print() {
+		if (vehicle == null) {
+			if (spotSize == VehicleSize.Compact) {
+				System.out.print("compact");
+			} else if (spotSize == VehicleSize.Large) {
+				System.out.print("large");
+			} else if (spotSize == VehicleSize.Motorcycle) {
+				System.out.print("motorcycle");
+			}
+		} else {
+			vehicle.print();
+		}
 	}
 
 }
